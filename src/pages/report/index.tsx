d5bef8a074fb88af7bdaff8330a7e131e7b7fcf4
Shubham-2899 <shubhamlatake123@gmail.com>
2024-11-16 01:47:36 +0530

    initial commit for report page refactoring

--- src/pages/report/index.tsx
+++ src/pages/report/index.tsx
@@ -1,58 +1,72 @@
 import React, { useEffect, useState } from "react";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Typography,
   Container,
   CircularProgress,
   Alert,
   Box,
+  Typography,
 } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { apiGet } from "../../utils/api";
 
-interface ReportData {
+interface IReport {
   offerId: string;
+  campaignId: string;
+  clickCount: number;
+  totalEmailSent: number;
   date: string;
-  offerName: string;
-  totalClicks: number;
-  affiliate: string;
-  revenue: number;
-  expired: boolean;
-  emailSentCount: number;
 }
 
 const Report: React.FC = () => {
-  const [reportData, setReportData] = useState<ReportData[]>([]);
+  const [reportData, setReportData] = useState<IReport[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 10,
+  });
+  const [totalElements, setTotalElements] = useState<number>(0);
+
+  const fetchReportData = async (
+    currentPage: number,
+    currentPageSize: number
+  ) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await apiGet(
+        `/reports?page=${currentPage + 1}&pageSize=${currentPageSize}`
+      );
+      setReportData(response.data.reports);
+      setTotalElements(response.data.totalElements);
+      setLoading(false);
+    } catch (error) {
+      setError("Error fetching report data");
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchReportData = async () => {
-      try {
-        const response = await apiGet("/url/reports");
-        // const response = await axios.get(
-        //   `${import.meta.env.VITE_APP_API_BASE_URL}/url/reports`,
-        //   {
-        //     headers: {
-        //       Authorization: `Bearer ${token}`,
-        //     },
-        //   }
-        // );
-        setReportData(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError("Error fetching report data");
-        setLoading(false);
-      }
-    };
+    fetchReportData(paginationModel.page, paginationModel.pageSize);
+  }, [paginationModel]);
 
-    fetchReportData();
-  }, []);
+  const columns: GridColDef[] = [
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+      valueGetter: (params) => new Date(params).toLocaleString(),
+    },
+    { field: "offerId", headerName: "Offer ID", flex: 1, maxWidth: 250 },
+    { field: "campaignId", headerName: "Campaign ID", flex: 1, maxWidth: 250 },
+    { field: "clickCount", headerName: "Click Count", flex: 1, maxWidth: 250 },
+    {
+      field: "totalEmailSent",
+      headerName: "Total Emails Sent",
+      flex: 1,
+      maxWidth: 250,
+    },
+  ];
 
   if (loading) {
     return (
@@ -71,48 +85,25 @@ const Report: React.FC = () => {
   }
 
   return (
-    <Box sx={{ mt: "20px" }}>
-      <Typography
-        variant="h4"
-        gutterBottom
-        // color="primary"
-        sx={{ textAlign: "center" }}
-      >
+    <Box sx={{ mt: "20px", width: "100%" }}>
+      <Typography variant="h5" gutterBottom sx={{ textAlign: "center", mb: 2 }}>
         Report
       </Typography>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead sx={{ bgcolor: "lightgray" }}>
-            <TableRow>
-              <TableCell>Date</TableCell>
-              <TableCell>Affiliate</TableCell>
-              <TableCell>Offer ID</TableCell>
-              <TableCell>Offer Name</TableCell>
-              <TableCell>Email Sent</TableCell>
-              <TableCell>Total Clicks</TableCell>
-              <TableCell>Revenue</TableCell>
-              <TableCell>Expired</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {reportData.map((data, index) => (
-              <TableRow
-                key={index}
-                sx={{ bgcolor: index % 2 != 0 ? "lightblue" : "" }}
-              >
-                <TableCell>{data?.date || "NA"}</TableCell>
-                <TableCell>{data?.affiliate || "NA"}</TableCell>
-                <TableCell>{data.offerId}</TableCell>
-                <TableCell>{data?.offerName || "TEST"}</TableCell>
-                <TableCell>{data?.emailSentCount || 0}</TableCell>
-                <TableCell>{data.totalClicks}</TableCell>
-                <TableCell>{data.revenue || "$" + 0}</TableCell>
-                <TableCell>{data.expired ? "Yes" : "No"}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <DataGrid
+        rows={reportData.map((report, index) => ({
+          id: index + paginationModel.page * paginationModel.pageSize,
+          ...report,
+        }))}
+        columns={columns}
+        disableColumnFilter
+        disableColumnMenu
+        rowCount={totalElements}
+        pagination
+        pageSizeOptions={[20, 50, 100]}
+        paginationMode="server"
+        paginationModel={paginationModel}
+        onPaginationModelChange={(newModel) => setPaginationModel(newModel)}
+      />
     </Box>
   );
 };
